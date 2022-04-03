63e41438b43a002049af7849e2341717a3f7f9e7
marcos <marcosmartinz176@gmail.com>
2022-04-03 12:53:47 -0400

    Starting off with the react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const initialState = {
+  count: 0,
+  name: "Count",
+};
 
 function App() {
+  // Number is the state for whole app
+  // State goes in an array
+  const [number, setNumber] = useState(initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {number.name} : {number.count}
+      </h1>
+      <button>Add</button>
+      <button>Subtract</button>
     </div>
   );
 }
