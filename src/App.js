20583e38a45c5f674685f32f049f18fcf363f827
marcos <marcosmartinz176@gmail.com>
2022-04-03 14:10:37 -0400

    useEffect practice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import logo from "./logo.svg";
 import "./App.css";
@@ -13,13 +13,28 @@ function App() {
   // State goes in an array
   const [number, setNumber] = useState(initialState);
 
+  const add = () => {
+    setNumber({ ...number, count: number.count + 1 });
+  };
+  const subtract = () => {
+    setNumber({ ...number, count: number.count - 1 });
+  };
+
+  useEffect(() => {
+    console.log("Number has changed");
+  }, [number]);
+
+  useEffect(() => {
+    console.log("App has rendered");
+  }, []);
+
   return (
     <div className="App">
       <h1>
         {number.name} : {number.count}
       </h1>
-      <button>Add</button>
-      <button>Subtract</button>
+      <button onClick={add}>Add</button>
+      <button onClick={subtract}>Subtract</button>
     </div>
   );
 }
